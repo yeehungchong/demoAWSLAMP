bc0a9aecb887a9ae787a89e600dad6c205b00710
YEE HUNG CHONG <64572811+yeehungchong@users.noreply.github.com>
2022-08-05 12:17:56 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>>>> DEMO >>></title>
+	</head>
+	<body>
+		<p>>>>>>>>>>> CONTACT.PHP >>>>>>>>>></p>
+		<ul>
+			<li><a href="index.php">index.php</a></li>
+			<li><a href="about.php">about.php</a></li>
+			<li><a href="contact.php">contact.php</a></li>
+		</ul>
+	</body>
+</html>
